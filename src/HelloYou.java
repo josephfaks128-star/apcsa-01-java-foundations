cbc8326a109512da4b8617fdb252af754b23719f
josephfaks128-star <joseph_faks128@flatbush.org>
2026-09-16 20:26:00 +0000

    completed SO3 classwork

--- src/HelloYou.java
+++ src/HelloYou.java
@@ -2,16 +2,17 @@
  * Exercise 1 — HelloYou
  *
  * TODO: Print three separate lines:
- *   1. Your full name
+ *   1. Your name
  *   2. Your grade
  *   3. One sentence about yourself
+ * 
  *
  * Remember: the filename must match the class name exactly.
  */
 public class HelloYou {
     public static void main(String[] args) {
-
-        // Your code here
-
+         System.out.println("Joey Faks");
+         System.out.println("10th grade");
+         System.out.println("I love school");
     }
 }
